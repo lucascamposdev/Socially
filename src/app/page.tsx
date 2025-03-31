3a0cf7426f03da12494a1ee81dcfd21e3f9249f1
Lucas <prestoinker@gmail.com>
2025-03-31 17:23:16 -0300

    feat: navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,3 @@
-import ThemeToggler from "@/components/ThemeToggler"
-import {
-  SignedIn,
-  SignedOut,
-  SignInButton,
-  SignUpButton,
-  UserButton
-} from "@clerk/nextjs"
-
 export default function Home() {
-  return (
-    <div>
-      <SignedOut>
-        <SignInButton />
-      </SignedOut>
-
-      <SignedIn>
-        <UserButton />
-      </SignedIn>
-
-
-    <ThemeToggler/>
-    </div>
-  )
+  return <div></div>
 }
