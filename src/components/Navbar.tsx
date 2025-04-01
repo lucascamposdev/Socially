382d65e12bc1495361fa7330a4a048434031a320
Lucas <prestoinker@gmail.com>
2025-04-01 01:34:32 -0300

    feat: auth & db sync

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,20 +1,22 @@
-
-import Link from "next/link";
-import { currentUser } from "@clerk/nextjs/server";
-import DesktopNavbar from "./DesktopNavbar";
-import MobileNavbar from "./MobileNavbar";
-// import { syncUser } from "@/actions/user.action";
+import Link from "next/link"
+import { currentUser } from "@clerk/nextjs/server"
+import DesktopNavbar from "./DesktopNavbar"
+import MobileNavbar from "./MobileNavbar"
+import { syncUser } from "@/actions/user.action"
 
 async function Navbar() {
-  const user = await currentUser();
-//   if (user) await syncUser(); // POST
+  const user = await currentUser()
+  if (user) await syncUser() // POST
 
   return (
     <nav className="sticky top-0 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60 z-50">
       <div className="max-w-7xl mx-auto px-4">
         <div className="flex items-center justify-between h-16">
           <div className="flex items-center">
-            <Link href="/" className="text-xl font-bold text-primary font-mono tracking-wider">
+            <Link
+              href="/"
+              className="text-xl font-bold text-primary font-mono tracking-wider"
+            >
               Socially
             </Link>
           </div>
@@ -24,6 +26,6 @@ async function Navbar() {
         </div>
       </div>
     </nav>
-  );
+  )
 }
-export default Navbar;
+export default Navbar
